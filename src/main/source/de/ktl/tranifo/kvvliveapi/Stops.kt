9b77cd6988c879753d8e7bb183f182d59b541009
Julia Burgard <burgard@synyx.de>
2018-06-29 13:20:49 +0200

    Improve ugly array iteration of the jsonarray while laoding the stops

--- src/main/source/de/ktl/tranifo/kvvliveapi/Stops.kt
+++ src/main/source/de/ktl/tranifo/kvvliveapi/Stops.kt
@@ -1,30 +1,16 @@
 package de.ktl.tranifo.kvvliveapi
 
 import khttp.get
-import java.util.*
 
 /**
  * @author  Julia Burgard - burgard@synyx.de
  */
-fun getStops(lat: String, lon: String): ArrayList<Stop> {
-    val jsonObject = get("https://live.kvv.de/webapp/stops/bylatlon/$lat/$lon" +
-            "?key=$KEY").jsonObject
+fun getStops(lat: String, lon: String): List<Stop> {
+
+    val jsonObject = get("https://live.kvv.de/webapp/stops/bylatlon/$lat/$lon?key=$KEY").jsonObject
+
     val stops = jsonObject.getJSONArray("stops")
-    println(jsonObject)
-
-    var i = 0
-    val numIterations = stops.length()
-    val stopObjects = ArrayList<Stop>();
-    while (i < numIterations) {
-        val obj = stops.getJSONObject(i)
-        val stop = Stop(obj.getString("id"), obj.getString("name"), obj.getInt("distance"))
-        stopObjects.add(stop)
-        i++
-
-    }
-
-    return stopObjects;
-
+    return stops.toList().map { obj -> Stop(obj.getString("id"), obj.getString("name"), obj.getInt("distance")) };
 }
 
 data class Stop(val id: String, val name: String, val distance: Int) {
