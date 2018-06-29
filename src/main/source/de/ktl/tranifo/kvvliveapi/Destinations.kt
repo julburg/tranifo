640383bd06f49112cf18ea82a340d0e6120ea0bb
Julia Burgard <burgard@synyx.de>
2018-06-29 13:18:35 +0200

    Use departure method to get all the destinations for a route and stop

--- src/main/source/de/ktl/tranifo/kvvliveapi/Destinations.kt
+++ src/main/source/de/ktl/tranifo/kvvliveapi/Destinations.kt
@@ -1,25 +1,12 @@
 package de.ktl.tranifo.kvvliveapi
 
-import khttp.get
-
 /**
  * @author  Julia Burgard - burgard@synyx.de
  */
 
-fun getDestinations(route:String, stopId: String): Set<String> {
+fun getDestinations(route: String, stopId: String): Set<String> {
 
-    val jsonObject = get("https://live.kvv.de/webapp/departures/byroute/$route/$stopId" +
-            "?maxInfos=10&key=$KEY").jsonObject
-    val departures = jsonObject.getJSONArray("departures")
-    println(jsonObject)
-    var i = 0
-    val numIterations = departures.length()
-    val destinations = HashSet<String>();
-    while (i < numIterations) {
-        destinations.add(departures.getJSONObject(i).getString("destination"))
-        i++
-
-    }
-    return destinations
+    val departures1 = getDepartures(route, stopId);
+    return departures1.map { departure -> departure.destination }.toSet()
 }
 
