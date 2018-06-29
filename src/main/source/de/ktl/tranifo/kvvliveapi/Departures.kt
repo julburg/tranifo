8e862e9ad6de2ed5dda7432a75dace23b57994c5
Julia Burgard <burgard@synyx.de>
2018-06-29 13:18:15 +0200

    Improve ugly array iteration of the jsonarray while laoding the departures

--- src/main/source/de/ktl/tranifo/kvvliveapi/Departures.kt
+++ src/main/source/de/ktl/tranifo/kvvliveapi/Departures.kt
@@ -1,30 +1,20 @@
 package de.ktl.tranifo.kvvliveapi
 
 import khttp.get
-import java.util.*
+import org.json.JSONArray
+import org.json.JSONObject
 
 /**
  * @author  Julia Burgard - burgard@synyx.de
  */
 
-fun getDepartures(route: String, stopId: String): ArrayList<Departure> {
+fun getDepartures(route: String, stopId: String): List<Departure> {
 
-  val jsonObject = get("https://live.kvv.de/webapp/departures/byroute/$route/$stopId" +
+    val jsonObject = get("https://live.kvv.de/webapp/departures/byroute/$route/$stopId" +
             "?maxInfos=10&key=$KEY").jsonObject
 
     val departures = jsonObject.getJSONArray("departures")
-    println(jsonObject)
-    var i = 0
-    val numIterations = departures.length()
-    val departureObjects = ArrayList<Departure>();
-    while (i < numIterations) {
-        val obj = departures.getJSONObject(i)
-        val departure = Departure(obj.getString("route"), obj.getString("destination"), obj.getString("time"), obj.getBoolean("realtime"))
-        departureObjects.add(departure)
-        i++
-
-    }
-    return departureObjects
+    return departures.toList().map { obj -> Departure(obj.getString("route"), obj.getString("destination"), obj.getString("time"), obj.getBoolean("realtime")) };
 }
 
 data class Departure(val route: String, val destination: String, val time: String, val realtime: Boolean) {
@@ -33,3 +23,5 @@ data class Departure(val route: String, val destination: String, val time: Strin
         return route + " " + destination + ": " + time
     }
 }
+
+fun JSONArray.toList(): List<JSONObject> = (0 until length()).asSequence().map { get(it) as JSONObject }.toList()
