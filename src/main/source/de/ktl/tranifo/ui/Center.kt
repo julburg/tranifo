16afa0b31a1bd472bcec8833ed4f7f68dbf49663
Julia Burgard <burgard@synyx.de>
2018-06-29 10:16:42 +0200

    Triing to get the current lat lon coordinates with a LookupService
    * its not working yet because the coordinates are not exact enough

--- src/main/source/de/ktl/tranifo/ui/Center.kt
+++ src/main/source/de/ktl/tranifo/ui/Center.kt
@@ -1,5 +1,6 @@
 package de.ktl.tranifo.ui
 
+import com.maxmind.geoip.LookupService
 import de.ktl.tranifo.kvvliveapi.Stop
 import de.ktl.tranifo.kvvliveapi.getStops
 import javafx.beans.property.SimpleObjectProperty
@@ -7,14 +8,20 @@ import javafx.geometry.Insets
 import javafx.scene.control.SelectionMode
 import tornadofx.*
 
+
 /**
  * @author  Julia Burgard - burgard@synyx.de
  */
 class Center() : View() {
 
     override val root = vbox {
-        val stops = getStops("49.0040079", "8.3849635")
+        val cl = LookupService("/Users/juliaburgard/Documents/GeoLiteCity.dat",
+                LookupService.GEOIP_MEMORY_CACHE or LookupService.GEOIP_CHECK_CACHE)
+        val location = cl.getLocation("109.109.203.66")
+        println(location.longitude.toString())
+        println(location.latitude.toString())
 
+        val stops = getStops(location.latitude.toString(), location.longitude.toString())
 
         val stop = SimpleObjectProperty<Stop>()
         fieldset("Stop Information") {
